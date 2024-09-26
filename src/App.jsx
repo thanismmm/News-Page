2865d48b193b917114113e0b69e6eccbd226f648
Mohammed Thanis <thanis7168t@gmail.com>
2024-09-26 18:45:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Bottompage from "./bottompage";
+
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [squeryn, setSquery] = useState("Cyber Crime");
+
+  const handeChange = (e) => {
+    setSquery(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchNews = () => {
+      fetch(`http://hn.algolia.com/api/v1/search?query=${squeryn}`)
+        .then((result) => result.json())
+        .then((data) => setNews(data.hits))
+        .catch((error) => console.log(error));
+    };
+
+    fetchNews();
+  }, [squeryn]);
+
+  return (
+    <div className="news">
+      <h1>News Page</h1>
+      <input type="text" value={squeryn} onChange={handeChange} />
+      <div className="news-content">
+        {news.map((n, i) => (
+          <p className="title" key={i}>
+            {n.title}
+            {n.url && (
+              <a href={n.url} target="_blank" rel="noopener noreferrer">
+                Go to Article
+              </a>
+            )}
+          </p>
+        ))}
+      </div>
+      <Bottompage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Bottompage.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Bottompage = () => {
+  return (
+    <div className="bottom">
+      <div className="content">
+        <div className="left">
+          <h3>News Page</h3>
+          <p>
+            Timely, accurate, unbiased. Our dedicated team of journalists is
+            committed to providing you with the information you need to make
+            informed decisions.
+          </p>
+          <h5>076 3075 074</h5>
+          <h5>thanis@gmail.com</h5>
+        </div>
+        <div className="right">
+          <p>Usefull Website Links</p>
+          <ul>
+            <div className="link-item">
+              <a href="https://www.nytimes.com/" target="_balnk">
+                The New York Times
+              </a>
+            </div>
+            <div className="link-item">
+              <a href="https://www.theguardian.com/" target="_balnk">
+                The Guardian
+              </a>
+            </div>
+            <div className="link-item">
+              <a href="https://www.bbc.com/" target="_balnk">
+                BBC News
+              </a>
+            </div>
+            <div className="link-item">
+              <a href="https://www.reuters.com/" target="_balnk">
+                Reuters
+              </a>
+            </div>
+            <div className="link-item">
+              <a href="https://www.cnn.com/" target="_balnk">
+                CNN
+              </a>
+            </div>
+          </ul>{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bottompage;
